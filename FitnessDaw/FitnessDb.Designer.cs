a14a864c4ffc27adaedcf8ac2be1d7b1ae506491
Esteban Muñoz <edmunozg@gmail.com>
2013-08-17 00:58:25 -0500

    creacion de las entidades y archivo sql de la base de datos

---
+++ FitnessDaw/FitnessDb.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Esteban\Desktop\Trabajos\Desarrollo de aplicaciones web\FitnessDaw\FitnessDaw\FitnessDb.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
