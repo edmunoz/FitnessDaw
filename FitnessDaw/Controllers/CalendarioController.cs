a109c694fde6cfc6c37194c48234bfbcd4172083
Esteban Muñoz <edmunozg@gmail.com>
2013-08-19 00:56:59 -0500

    new controllers

--- FitnessDaw/Controllers/CalendarioController.cs
+++ FitnessDaw/Controllers/CalendarioController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,13 +10,118 @@ namespace FitnessDaw.Controllers
 {
     public class CalendarioController : Controller
     {
+        private FitnessDbEntities1 db = new FitnessDbEntities1();
+
         //
         // GET: /Calendario/
 
         public ActionResult Index()
         {
+            var calendarios = db.calendarios.Include(c => c.usuario);
+            return View(calendarios.ToList());
+        }
+
+        //
+        // GET: /Calendario/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            calendario calendario = db.calendarios.Find(id);
+            if (calendario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendario);
+        }
+
+        //
+        // GET: /Calendario/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nick");
             return View();
         }
 
+        //
+        // POST: /Calendario/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(calendario calendario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.calendarios.Add(calendario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nick", calendario.idUsuario);
+            return View(calendario);
+        }
+
+        //
+        // GET: /Calendario/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            calendario calendario = db.calendarios.Find(id);
+            if (calendario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nick", calendario.idUsuario);
+            return View(calendario);
+        }
+
+        //
+        // POST: /Calendario/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(calendario calendario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(calendario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nick", calendario.idUsuario);
+            return View(calendario);
+        }
+
+        //
+        // GET: /Calendario/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            calendario calendario = db.calendarios.Find(id);
+            if (calendario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendario);
+        }
+
+        //
+        // POST: /Calendario/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            calendario calendario = db.calendarios.Find(id);
+            db.calendarios.Remove(calendario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
---
+++ FitnessDaw/Controllers/UsuarioController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessDaw.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private FitnessDbEntities1 db = new FitnessDbEntities1();
+
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View(db.usuarios.ToList());
+        }
+
+        //
+        // GET: /Usuario/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Usuario/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            db.usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
