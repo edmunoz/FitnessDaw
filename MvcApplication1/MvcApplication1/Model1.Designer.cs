8431f333312ae16c51eabb5060b17db9c9c0e401
Esteban Muñoz <edmunozg@gmail.com>
2013-09-06 11:00:08 -0500

    Ultimos cambios diseno implementado con sus respectivas pestanas

--- MvcApplication1/MvcApplication1/Model1.Designer.cs
+++ MvcApplication1/MvcApplication1/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Esteban\Documents\GitHub\FitnessDaw\MvcApplication1\MvcApplication1\Models\ModelFitness.edmx'. 
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Esteban\Documents\GitHub\FitnessDaw\MvcApplication1\MvcApplication1\Model1.edmx'. 
 // Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
